a090cb27b45634c6d1b4b115bc59ca9fac96a156
@chhpoirier <catherinepoirier@comcast.net>
2021-04-06 10:54:51 -0600

    added readme and cleaned up comments

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # plotly-challenge
+Belly Button Biodiversity
+
+
+I build an interactive dashboard that shows the Belly Button Biodiversity regarding microbes that colonize human navels.
+The results reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+The following steps occured to created this dashboard.
+Read in a .json data file that was provided with the results of this study.
+Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+The bar chart has hover text over the data that shows the OTU Labels.
+Th bubble chart has different colors based on thte OTU_ids and sizes based on the sample Values.
+There is also a demographic chart which displays the selected indivuidals demographic information: id, ethnicity, gender, age, olcation bbtype, and wfreq.
+All graphs and charts update to each time a new subject's id is selected.
+
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,11 @@
 // 1. Use the D3 library to read in samples.json.
 // Fetch the JSON data and console log it
-// d3.json('../../samples.json').then(data=>{
-//     console.log(data)});
 function init(){
   d3.json("../../samples.json").then((data)=>
   {
       var names = data.names;
-  //   console.log(names);
       var samples_array=data.samples;
       console.log(samples_array);
-        // var samples_current=samples_array.filter((picked)=>picked.id==optiontag);
-        // console.log(samples_current);
-      // var metadata_array=data.metadata;
-      // console.log(metadata_array);
 
   //creates elements for dropdown menu
     var select = document.getElementById("selDataset"); 
@@ -23,26 +16,18 @@ function init(){
       el.value = opt;
       select.appendChild(el);
       }
-
     buildgraph(names[0]);
 
     });
-  }
+} //closes init function
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // Use the first sample from the list to build the initial plots
 
 function buildgraph(selectedid){
   d3.json("../../samples.json").then((data)=>{
 
-    // var selectedid=samples_array.filter(filterBBdata);
     var otuidarray = data.samples.filter(eachelement=>eachelement.id===selectedid);
     var selected_person=otuidarray[0];
-    
-
-    // var otuidstring = otuid.toString();  // console.log(otuid);  //  console.log(otuidstring);
-   //     var sample_otu_id=selectedid.map(oneElement=>oneElement['otu_ids']);
-//     var sample_otu_id_string=sample_otu_id.toString();  
-    
     var otuid =selected_person["otu_ids"]; 
     var sample_otu_id=otuid.map(oneElement=>oneElement.toString()); 
     //console.log(sample_otu_id);
@@ -100,161 +85,7 @@ function buildgraph(selectedid){
       demotable.text(`${key}: ${val}`);
           }
         );
-    
-
-
-
-      // for (var i=0; i<data.length; i++){   
-      //     var rowrow=demorow.append('tr');
-      //     current_data=data[i];
-      //     value_list=Object.values(current_data);
-      //     for (var x=0; x<value_list.length; x++){
-      //         rowrow.append('td').text(value_list[x]);
-      //     };
-      // };
-
-
-      
-// ==========================================
 
   }) //end of d3 then statement
 } //end of build graph function
-
-// function runEnter() {
-//   tbody.html("");
-//   d3.event.preventDefault();    //Prevent the page from refreshing
-//   var inputElement = d3.select("#datetime");   //Select the input element and get the raw HTML node
-
-//   var inputValue = inputElement.property("value");   //Get the value property of the input element
-
-//   console.log(inputValue);
-// //  console.log(tableData);
-//   var filteredData = tableData.filter(day => day.datetime === inputValue);
-  
-//   for (var i=0; i<filteredData.length; i++){   
-//   var rowrow=tbody.append('tr');
-//   current_data2=filteredData[i];
-//   value_list=Object.values(current_data2);
-//   for (var x=0; x<value_list.length; x++){
-//       rowrow.append('td').text(value_list[x]);
-//   };
-// };
-//   console.log(filteredData);
-// }
-
-
-
-
-
 init();
-   
-//       // }
-// //BONUS -Adapt the Gauge Chart from https://plot.ly/javascript/gauge-charts/ to plot the weekly washing frequency of the individual.
-// //You will need to modify the example gauge code to account for values ranging from 0 through 9.
-// //Update the chart whenever a new sample is selected.   
-
-//      var mdid = data.metadata[0]["id"];
-//      var mdwfeq= data.metadata[0]["wfeq"]; 
-      
-//      var trace3 = [
-//       {
-//         domain: { x: [0, 9], y: [0, 9]  },
-//         value: mdwfeq,
-//         title: { text: "Belly Button Washing Frequency, Scrubs per Week" },
-//         type: "indicator",
-//         mode: "gauge+number"
-//       }
-//     ];
-//         var layout2 = { width: 400, height: 300, margin: { t: 0, b: 0 } };
-//     Plotly.newPlot('gauge', trace3, layout2); 
-
-//     // 6. Update all of the plots any time that a new sample is selected
-
-//  function buildgraph(id)
-
-//     var selectedid=samples_array.filter(filterBBdata);
-//     var sample_otu_id=selectedid.map(oneElement=>oneElement['otu_ids']);
-//     var sample_otu_id_string=sample_otu_id.toString();
-//     var sample_V_info=selectedid.map(oneElement=>oneElement['sample_values']);
-//     var sample_otu_labels=selectedid.map(oneElement=>oneElement['otu_labels']);
-    
-//     console.log(selectedid);
-
-//     }); //closes
-//  // function optionChanged(optiontag){
-// //   console.log(optiontag);
-// // }
-
-// // var selectedid=samples_array.filter(filterBBdata);
-// // var sample_otu_id=selectedid.map(oneElement=>oneElement['otu_ids']);
-// // var sample_otu_id_string=sample_otu_id.toString();
-// // var sample_V_info=selectedid.map(oneElement=>oneElement['sample_values']);
-// // var sample_otu_labels=selectedid.map(oneElement=>oneElement['otu_labels']);
-
-// //console.log(selectedid);
-
-//   //var otuids =data.samples.one_id.map(one_id=>one_id[i] // not what i want
-
-// // var otuid = data.samples[0]["otu_ids"].slice(0, 10);
-// //     var otuidstring = otuid.toString();  // console.log(otuid);  //  console.log(otuidstring);
-// //     var sampleV = data.samples[0]["sample_values"].slice(0, 10);   //console.log(sampleV);
-// //     var otuLl = data.samples[0]["otu_labels"].slice(0, 10);  //console.log(otuLl);
-    
-// // 
-// // function buildMetadata(sample) {
-// //   d3.json("../../samples.json").then((data) => {
-// //     var metadata = data.metadata;
-// //     console.log(metadata)
-// //   })};
-
-// // function init() {
-// //     // Grab a reference to the dropdown select element
-// //     var selector = d3.select("#selDataset"); // Use the list of sample names to populate the select options
-// //      d3.json("../../samples.json").then((data) => {
-// //       var sampleNames = data.names; 
-// //         sampleNames.forEach((sample) => {
-// //             selector.append("option").text(sample).property("value", sample);
-// //         }); 
-// //      }
-    
-
-// // //Plotly.restyle("bar", "x", [datasets[dataset]['x']]);
-// // //Plotly.restyle("bar", "y", [datasets[dataset]['y']]);
-// // //restyle only changes the x axis or the y axis, one at at time
-// // //}
-
-
-// // 4. Display the sample metadata, i.e., an individual's demographic information.
-// // function buildMetadata(sample) {
-// //   d3.json("samples.json").then((data) => {
-// //     var metadata = data.metadata;
- 
-// //     demo_info.html("");  //Clears the entries from previous entry
-
-
-
-//     // var mdid = data.metadata[0]["id"];
-//     // var mdethnicity = data.metadata[0]["ethnicity"];
-//     // var mdgender = data.metadata[0]["gender"];
-//     // var mdage= data.metadata[0]["age"];
-//     // var mdlocation= data.metadata[0]["location"];
-//     // var mdbbtype= data.metadata[0]["bbtype"];
-//     // var mdwfeq= data.metadata[0]["wfeq"];
-//     // console.log(mdid);
-//     // console.log(mdethnicity);
-//     // console.log(mdgender);
-//     // console.log(mdlocation);
-//     // console.log(mdage);
-//     // console.log(mdbbtype);
-//     // console.log(mdwfeq);
-
-//     // function optionChanged(optiontag){
-// //     console.log(optiontag);
-// // }
-
-// var tbody=d3.select("tbody");
-//       tbody.html("")
-//       object.entries(selected_metadata).forEach(([k, v])=>{
-//         var row=tbody.append("tr");
-//         row.text(`${k}: ${v}`);
-//       });
